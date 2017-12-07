aa01995a65ca4c8d2bc1e869e0edb96cac65a3dd
Jamie Burke <jamie@ntslive.co.uk>
2017-12-07 12:49:06 +0000

    Font sizing, spacing and responsiveness of left panel on landing done.

--- src/components/GalaxyInfo/GalaxyInfo.js
+++ src/components/GalaxyInfo/GalaxyInfo.js
@@ -7,26 +7,27 @@ import '../../../sass/_settings.scss';
 const GalaxyInfo = () => (
     <div id="galaxy-info">
         <div className="galaxy-info__header">
-            <span>NTS Radio</span>
-            <br/>
-            <span>in collaboration with NASA</span>
-            <br/>
-            <span>presents</span>
+            <a className="galaxy-info__header__link" href="https://www.nts.live" target="_blank"><img src="/nts-logo-white.png"/></a>
+
+            <div className="galaxy-info__header__text">
+                <span>NTS Radio</span>
+                <br/>
+                <span>in collaboration with NASA</span>
+                <br/>
+                <span>presents</span>
+            </div>
         </div>
 
         <div className="galaxy-info__content">
-            <h1><span>Jenny Maya:</span> The Outer Limits</h1>
-            <h3>5 part radio series with original music production</h3>
+            <h1 className="title text-uppercase"><span className="title__name">Jenny Maya:</span>The Outer Limits</h1>
+            <h3 className="subtitle">5 part radio series with original music production</h3>
         </div>
 
         <div className="galaxy-info__footer">
-            <span>Next broadcast</span>
-            <br />
-            <span>on NTS</span>
-            <br />
-            <span>01.01.18</span>
-            <br />
-            <span>TIME 13:00</span>
+            <div>Next broadcast</div>
+            <div>on NTS</div>
+            <div style={{fontSize: '28px', marginTop: '13px'}}>01.01.18</div>
+            <div style={{fontSize: '12px'}}>TIME 13:00</div>
         </div>
     </div>
 );
