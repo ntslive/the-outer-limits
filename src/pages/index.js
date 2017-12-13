40939eca1bba088e89fd90a965fa272d8197dd7b
Jamie Burke <jamie@ntslive.co.uk>
2017-12-13 11:49:13 +0000

    Removed unused components from initialy gatsby template.:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 /* global graphql */
 
 import React from 'react';
-import Features from '../components/features';
-import HowTo from '../components/how-to';
 
 import Galaxy from '../components/Galaxy/Galaxy';
 
 const IndexPage = props =>
     (<main>
-        {/*<Features data={props.data.allDataJson.edges[0].node.features} />*/}
-        {/*<HowTo data={props.data.allDataJson.edges[0].node.howTo} />*/}
         <Galaxy chapters={props.data.allDataJson.edges[0].node.chapters} />
         <div className="background-image" />
     </main>);
