3d46d739a1b893711eb65db3a51ea9b43e19ae0b
andistuder <andi@ntslive.co.uk>
2017-12-19 13:16:56 +0000

    remove console.log

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Player = (props) => {
     const trackID = props.trackID;
     const secretToken = props.secretToken;
-    console.log('prop', props);
 
     let iframeSrcUrl = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + trackID
     if (secretToken && secretToken.length > 0) {
@@ -15,9 +14,5 @@ const Player = (props) => {
         <iframe width="100%" height="20" scrolling="no" frameBorder="no" src={iframeSrcUrl}></iframe>
     );
 };
-
-// Player.propTypes = {
-//     trackID: PropTypes.number.isRequired,
-// };
 
 export default Player;
