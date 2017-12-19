e5fb3532bcf7f8cb57c1e861fd53c05b1f698a9f
andistuder <andi@ntslive.co.uk>
2017-12-19 16:24:09 +0000

    migrate to chapter ID being identical to the slug
    read chapter id from the location pathname

--- src/pages/chapters/apollo-18.js
+++ src/pages/chapters/apollo-18.js
@@ -3,13 +3,14 @@
 import React from 'react';
 import ChapterRouter from "../../components/utils/ChapterRouter";
 
-const pageChapterId = 1;
-
-const Apollo18 = props => (
-    <main>
-        <ChapterRouter chapters={props.data.allDataJson.edges[0].node.chapters} chapterId={pageChapterId} />
-    </main>
-);
+const Apollo18 = props => {
+    const chapterId = props.location.pathname.split('/')[2];
+    return (
+        <main>
+            <ChapterRouter chapters={props.data.allDataJson.edges[0].node.chapters} chapterId={chapterId} />
+        </main>
+    );
+};
 
 export default Apollo18;
 
