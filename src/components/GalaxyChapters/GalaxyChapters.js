e164ee9150fab72f8bd00a5fb5cf931ecb0a4e5e
Jamie Burke <jjaburke@hotmail.com>
2018-01-12 18:47:27 +0000

    Fixes for Edge and for IE.

--- src/components/GalaxyChapters/GalaxyChapters.js
+++ src/components/GalaxyChapters/GalaxyChapters.js
@@ -5,6 +5,41 @@ import './galaxy-chapters.scss';
 import GalaxyChapterStatusText from '../GalaxyChapterStatusText/GalaxyChapterStatusText';
 import chapterStatusManager from "../utils/chapterStatusManager";
 
+if (!String.prototype.repeat) { // polyfill for repeat function in IE
+    String.prototype.repeat = function(count) {
+        'use strict';
+        if (this == null) {
+            throw new TypeError('can\'t convert ' + this + ' to object');
+        }
+        var str = '' + this;
+        count = +count;
+        if (count != count) {
+            count = 0;
+        }
+        if (count < 0) {
+            throw new RangeError('repeat count must be non-negative');
+        }
+        if (count == Infinity) {
+            throw new RangeError('repeat count must be less than infinity');
+        }
+        count = Math.floor(count);
+        if (str.length == 0 || count == 0) {
+            return '';
+        }
+        // Ensuring count is a 31-bit integer allows us to heavily optimize the
+        // main part. But anyway, most current (August 2014) browsers can't handle
+        // strings 1 << 28 chars or longer, so:
+        if (str.length * count >= 1 << 28) {
+            throw new RangeError('repeat count must not overflow maximum string size');
+        }
+        var rpt = '';
+        for (var i = 0; i < count; i++) {
+            rpt += str;
+        }
+        return rpt;
+    }
+}
+
 function convertNumberToRomanNumeral(number) {
     let roman =  {"M" :1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1};
     let str = "";
