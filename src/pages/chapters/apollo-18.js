baced11b656f3fbf1bb306ec1980c883c8e5dc95
Jamie Burke <jjaburke@hotmail.com>
2018-03-08 11:43:51 +0000

    Created Chapter 2 route along with it's content.

---
+++ src/pages/chapters/apollo-18.js
@@ -0,0 +1,42 @@
+/* global graphql */
+
+import BaseChapter from "../../components/utils/BaseChapter";
+
+export default BaseChapter;
+
+export const pageQuery = graphql`
+  query ApolloQuery {
+    allDataJson {
+      edges {
+        node {
+          chapters {
+            id
+            name
+            broadcastDate
+            broadcastStartTime
+            broadcastEndTime
+            content {
+              description
+              image_bg
+              credits {
+                title
+                names
+              }
+            }
+            gallery {
+                title
+                description
+                credits
+                imageUrl
+            }
+            audio {
+              type
+              soundcloudSecretToken
+              soundcloudTrackID
+            }
+          }
+        }
+      }
+    }
+  }
+`;
