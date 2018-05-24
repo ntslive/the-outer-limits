d5e6b8ee863c07f0640b66eaa1467b706e0f35d3
Jamie Burke <jjaburke@hotmail.com>
2018-05-24 15:22:06 +0100

    Added all data for chapter 3 except for teaser audio.

---
+++ src/pages/chapters/star-people.js
@@ -0,0 +1,45 @@
+/* global graphql */
+
+import BaseChapter from "../../components/utils/BaseChapter";
+
+export default BaseChapter;
+
+export const pageQuery = graphql`
+  query StarPeopleQuery {
+    allDataJson {
+      edges {
+        node {
+          chapters {
+            id
+            name
+            broadcastDate
+            broadcastStartTime
+            broadcastEndTime
+            content {
+              description
+              credits {
+                title
+                names
+              }
+              qa {
+                title
+                paragraphs
+              }
+            }
+            gallery {
+                title
+                description
+                credits
+                imageUrl
+            }
+            audio {
+              type
+              soundcloudSecretToken
+              soundcloudTrackID
+            }
+          }
+        }
+      }
+    }
+  }
+`;
